0cc7cd8f32acb479c551fa1e5247cf10e3676e4a
Sanadam <sanadam@me.com>
2025-05-07 22:23:26 +0700

    add template

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/recover"
+	"github.com/gofiber/template/html/v2"
 	slogfiber "github.com/samber/slog-fiber"
 	"log"
+	"os"
 	"sanadam/sandbox-fiber/config"
 	"sanadam/sandbox-fiber/internal/derror"
 	"sanadam/sandbox-fiber/internal/pages"
@@ -15,7 +17,18 @@ func main() {
 	config.Init()
 
 	dErr := derror.NewDefaultErrorFiberConfig()
-	app := fiber.New(dErr)
+	engine := html.New("./html", ".html")
+	//engine.Templates.Funcs()
+	engine.Reload(true)
+
+	if _, err := os.Stat("./html"); os.IsNotExist(err) {
+		log.Fatal("Views folder does not exist")
+	}
+
+	app := fiber.New(fiber.Config{
+		Views:        engine,
+		ErrorHandler: dErr,
+	})
 	customLogger := logger.NewLogger()
 	app.Use(slogfiber.New(customLogger))
 	app.Use(recover.New())
--- internal/derror/handler.go
+++ internal/derror/handler.go
@@ -5,18 +5,16 @@ import (
 	"github.com/gofiber/fiber/v3"
 )
 
-func NewDefaultErrorFiberConfig() fiber.Config {
-	return fiber.Config{
-		ErrorHandler: func(c fiber.Ctx, err error) error {
-			code := fiber.StatusInternalServerError
+func NewDefaultErrorFiberConfig() fiber.ErrorHandler {
+	return func(c fiber.Ctx, err error) error {
+		code := fiber.StatusInternalServerError
 
-			var e *fiber.Error
-			if errors.As(err, &e) {
-				code = e.Code
-			}
-			return c.Status(code).JSON(fiber.Map{
-				"error": err.Error(),
-			})
-		},
+		var e *fiber.Error
+		if errors.As(err, &e) {
+			code = e.Code
+		}
+		return c.Status(code).JSON(fiber.Map{
+			"error": err.Error(),
+		})
 	}
 }
